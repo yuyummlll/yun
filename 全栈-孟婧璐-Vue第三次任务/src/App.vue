13d8302b60468e26dac0c819f4955dd69435ca7b
xiaokaoyuyuyu <1246287136@qq.com>
2025-04-13 21:42:54 +0800

    Add files via upload

---
+++ 全栈-孟婧璐-Vue第三次任务/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="app">
+
+    <Project title="热门歌手" :items="artists" class="artist">
+      <template v-slot:default="{ item }">
+        <div class="hotartist">
+          <img :src="item.picUrl" class="artist-photo" />
+          <span>{{ item.name }}</span>
+        </div>
+      </template>
+    </Project>
+    <h2 class="bangdan">榜单</h2>
+    <div class="flexapp">
+
+      <Project title="新歌榜" :cover-img-url="newsongs.coverImgUrl" :description="newsongs.description"
+        :items="newsongs.tracks">
+        <template v-slot:default="{ item, index }">
+          <div class="song-item">
+            <span>{{ index + 1 }}.</span>
+            <img :src="item.picUrl" class="song-photo" />
+            <div>
+              <span>{{ item.name }}</span>
+              
+            </div>
+          </div>
+        </template>
+      </Project>
+      <Project title="热歌榜" :items="hotsongs.tracks" :cover-img-url="hotsongs.coverImgUrl" :description="hotsongs.description">
+        <template v-slot:default="{ item, index }">
+          <div class="song-item">
+            <span>{{ index + 1 }}.</span>
+            <img :src="item.picUrl" class="song-photo" />
+            <div>
+              <span>{{ item.name }}</span>
+            </div>
+          </div>
+        </template>
+      </Project>
+
+
+      <Project title="原创榜" :cover-img-url="originsongs.coverImgUrl" :description="originsongs.description"
+        :items="originsongs.tracks">
+        <template v-slot:default="{ item, index }">
+          <div class="song-item">
+            <span>{{ index + 1 }}.</span>
+            <img :src="item.picUrl" class="song-photo" />
+            <div>
+              <span>{{ item.name }}</span>
+            </div>
+          </div>
+        </template>
+      </Project>
+    </div>
+  </div>
+</template>
+
+<script>
+import Project from './components/Project.vue'
+import artists from './assets/artist.js'
+import hotsongs from './assets/hotSong.js'
+import newsongs from './assets/newSong.js'
+import originsongs from './assets/originSong.js'
+
+export default {
+  components: { Project },
+  data() {
+    return {
+      artists,
+      hotsongs,
+      newsongs,
+      originsongs
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  width: 1300px;
+}
+
+
+.artist ul {
+  display: flex;
+  flex-wrap: nowrap;
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+
+}
+
+.hotartist {
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+}
+
+.flexapp {
+  display: flex;
+  justify-content: space-between;
+  padding:20px;
+}
+
+.bangdan {
+  padding: 20px;
+  border-bottom: 2px solid black;
+}
+
+.artist-photo {
+  width: 50px;
+  height: 50px;
+}
+
+.song-item {
+  display: flex;
+  align-items: center;
+}
+
+.song-photo {
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+
+}
+</style>
---
+++ 全栈-孟婧璐-Vue第三次任务/src/components/Project.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="project-container">
+
+        <div class="header">
+            <h2>{{ title }}</h2>
+            <img v-if="coverImgUrl" :src="coverImgUrl" class="cover-image" />
+        </div>
+        <p v-if="description" style="color:gray; border-bottom:2px solid black">{{ description }}</p>
+
+        <ul>
+            <li v-for="(item, index) in items" :key="index" class="list">
+                <slot :item="item" :index="index"></slot>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Project',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        items: {
+            type: Array,
+            required: true
+        },
+        coverImgUrl: {
+            type:String
+        },
+        picUrl: {
+            type: String,
+        },
+        description: {
+            type:String
+        },
+    },
+}
+</script>
+
+<style scoped>
+.project-container {
+    margin: 20px;
+}
+
+.header {
+    display: flex;
+    margin-left: 10px;
+}
+
+.cover-image {
+    width: 80px;
+    height: 80px;
+    margin-left: 80px;
+    padding: 20px;
+}
+
+.list {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border-bottom: 1px solid black;
+}
+</style>
